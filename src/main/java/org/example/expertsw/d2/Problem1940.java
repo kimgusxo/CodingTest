f0c52574d7b434533d833512a60c0c2cb1320d4f
kimgusxo <81300338+kimgusxo@users.noreply.github.com>
2024-05-17 04:51:27 +0900

    swea 풀이중

---
+++ src/main/java/org/example/expertsw/d2/Problem1940.java
@@ -0,0 +1,51 @@
+package org.example.expertsw.d2;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Problem1940 {
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+
+        int cnt = Integer.parseInt(bf.readLine());
+
+        for(int i=1; i<=cnt; i++) {
+            int sec = Integer.parseInt(bf.readLine());
+
+            int speed = 0;
+            int distance = 0;
+            int accel = 0;
+
+            for(int j=0; j<sec; j++) {
+                StringTokenizer st = new StringTokenizer(bf.readLine());
+
+                int command = Integer.parseInt(st.nextToken());
+
+                if(command!=0) {
+                    accel = Integer.parseInt(st.nextToken());
+                }
+
+                switch (command) {
+                    case 0:
+                        distance += speed;
+                        break;
+                    case 1:
+                        speed += accel;
+                        distance += speed;
+                        break;
+                    case 2:
+                        speed -= accel;
+                        if(speed<0) speed=0;
+                        distance += speed;
+                        break;
+                    }
+                }
+
+            System.out.println("#"+i+" "+distance);
+
+        }
+    }
+}
---
+++ src/main/java/org/example/expertsw/d2/Problem1945.java
@@ -0,0 +1,41 @@
+package org.example.expertsw.d2;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+
+public class Problem1945 {
+
+    static int[] factorization = {2, 3, 5, 7, 11};
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+
+        int cnt = Integer.parseInt(bf.readLine());
+
+        for(int i=1; i<=cnt; i++) {
+            int num = Integer.parseInt(bf.readLine());
+            ArrayList<Integer> arr = new ArrayList<>();
+
+            for(int a : factorization) {
+                int repeat = 0;
+                while(true) {
+                    if(num%a==0) {
+                        num /= a;
+                        repeat++;
+                    } else {
+                        break;
+                    }
+                }
+                arr.add(repeat);
+            }
+
+            System.out.print("#"+i+" ");
+            arr.stream().forEach((r) -> System.out.print(r+ " "));
+            System.out.println();
+        }
+
+    }
+}
