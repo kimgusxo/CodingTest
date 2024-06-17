567beaca94f6166e193834a5dee2c26902f0c7e5
kimgusxo <gusxoas9906@naver.com>
2024-06-17 17:36:44 +0900

    add programmers

---
+++ src/main/java/org/code/programmers/lv2/Main.java
@@ -0,0 +1,14 @@
+package org.code.programmers.lv2;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) {
+
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+
+
+
+    }
+}
