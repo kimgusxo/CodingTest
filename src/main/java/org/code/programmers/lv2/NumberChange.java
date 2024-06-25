c9f413e1df107726c7655f317dd99c637c20130a
kimgusxo <gusxoas9906@naver.com>
2024-06-25 17:41:56 +0900

    숫자 변경 문제 틀 작성

---
+++ src/main/java/org/code/programmers/lv2/NumberChange.java
@@ -0,0 +1,21 @@
+package org.code.programmers.lv2;
+
+
+public class NumberChange {
+    public static void main(String[] args) {
+
+        solution(10, 40, 5);
+        solution(10, 40, 30);
+        solution(2, 5, 4);
+
+    }
+
+    public static int solution(int x, int y, int n) {
+        int answer = 0;
+
+
+
+        return answer;
+    }
+
+}
