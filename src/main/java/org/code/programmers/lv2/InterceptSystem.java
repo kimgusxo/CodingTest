d9cba79d73abe04bfa4b50d41759484802308149
kimgusxo <gusxoas9906@naver.com>
2024-09-24 16:24:37 +0900

    체육복 문제풀이

---
+++ src/main/java/org/code/programmers/lv2/InterceptSystem.java
@@ -0,0 +1,16 @@
+package org.code.programmers.lv2;
+
+public class InterceptSystem {
+    public static void main(String[] args) {
+
+    }
+    public static int solution(int[][] targets) {
+        int answer = 0;
+
+        for(int[] target : targets) {
+            target[0]
+        }
+
+        return answer;
+    }
+}
